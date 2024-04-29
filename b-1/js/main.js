96730afa201b698971ca207b83fc367390f7da34
Kwonjaeseong <77simon@naver.com>
2024-04-29 17:00:36 +0900

    web

---
+++ b-1/js/main.js
@@ -0,0 +1,42 @@
+        $(function(){
+            // gnb
+            $(".gnb").hover(
+                function(){
+                    $(".gnb ul").stop().slideDown()
+                },
+                function(){
+                    $(".gnb ul").stop().slideUp()
+                }
+            )
+
+            // slider
+            setInterval(function(){
+                $(".sliders").animate({top : "-100%"}, function(){
+                    $(this).append($(".slide").eq(0))
+                    $(this).css("top", 0)
+                })
+            }, 3000)
+
+            // tab
+            $(".tab_title h4").click(function(){
+                $(".tab_title h4").removeClass("active")
+                $(this).addClass("active")
+
+                var idx = $(this).index()
+
+                $(".tab_contents ul").hide()
+                $(".tab_contents ul").eq(idx).show()
+                
+
+            })
+
+            //modal
+            $("#showModal").click(function(){
+                $(".modal").show()
+            })
+
+            $("#closeModal").click(function(){
+                $(".modal").hide()
+            })
+
+        })
