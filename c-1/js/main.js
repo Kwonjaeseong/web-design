ef40e817c90dfd2243ff1266142fc1a42b45efcf
Kwonjaeseong <77simon@naver.com>
2024-04-30 17:27:31 +0900

    web

---
+++ c-1/js/main.js
@@ -0,0 +1,45 @@
+$(function(){
+    //gnb
+    $(".gnb li").hover(
+        function(){
+            $(this).find(".sub").stop().slideDown()
+        },
+        function(){
+            $(this).find(".sub").stop().slideUp()
+        }
+    )
+
+    //slide
+
+    var index = 0;
+    $(".slide").eq(index).show()
+
+    setInterval(function(){
+        var nextIndex = (index + 1) % 3
+        $(".slide").eq(index).fadeOut()
+        $(".slide").eq(nextIndex).fadeIn()
+        index = nextIndex
+    }, 3000)
+
+    //tab
+    $(".tab_title h3").click(function(){
+        $(".tab_title h3").removeClass("active")
+        $(this).addClass("active")
+
+        var idx = $(this).index()
+        console.log(idx)
+
+        $(".tab_contents ul").hide()
+        $(".tab_contents ul").hide().eq(idx).show()
+
+    })
+
+    //modal
+    $("#showModal").click(function(){
+        $(".modal").show()
+    })
+
+    $("#closeModal").click(function(){
+        $(".modal").hide()
+    })
+})
